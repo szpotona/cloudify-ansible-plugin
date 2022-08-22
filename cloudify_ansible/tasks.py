90c7ed04f5f1863a1cfabf39126b5c0237f13f0f
Anna Szpoton <anna@Annas-MacBook-Pro.local>
2022-08-22 11:27:36 +0200

    Move precreate to configure

--- cloudify_ansible/tasks.py
+++ cloudify_ansible/tasks.py
@@ -270,7 +270,7 @@ def ansible_remove_host(new_sources_dict, _ctx, **_):
 
 @operation
 @prepare_ansible_node
-def precreate(ctx=None, **_):
+def configure(ctx=None, **_):
     ctx.logger.info('Checking Ansible installation.')
     if not utils.get_instance().runtime_properties.get(
             constants.PLAYBOOK_VENV):
